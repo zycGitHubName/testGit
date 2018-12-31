6eb249668e57ac0cc3b68e2104f99ecd24fb91d1
zycGitHubName <1923889599@qq.com>
2018-12-31 16:05:14 +0800

    查询所有用户的功能

--- src/main/java/cn/testgit/dao/UserDao.java
+++ src/main/java/cn/testgit/dao/UserDao.java
@@ -2,7 +2,11 @@ package cn.testgit.dao;
 
 import cn.testgit.pojo.User;
 
+import java.util.List;
+
 public interface UserDao {
 
     public User findUserByName(String name);
+
+    public List<User> findAll();
 }
--- src/main/java/cn/testgit/dao/impl/UserDaoImpl.java
+++ src/main/java/cn/testgit/dao/impl/UserDaoImpl.java
@@ -3,9 +3,16 @@ package cn.testgit.dao.impl;
 import cn.testgit.dao.UserDao;
 import cn.testgit.pojo.User;
 
+import java.util.List;
+
 public class UserDaoImpl implements UserDao {
     @Override
     public User findUserByName(String name) {
         return new User("admin","admin");
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
     }
 }
